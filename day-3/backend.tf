2297a303310ebf6cddbf310dfbfe99d1a33a2a94
akhilgoud8 <meruguakhilgoud98@gmail.com>
2026-07-15 16:53:26 +0530

    added

--- day-3/backend.tf
+++ day-3/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-11"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
+    bucket       = "my-terraform-state-11"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
   }
 }
