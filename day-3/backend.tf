3c02fb2d86c8c3c4ba0e22f9c808d8ea0f32b9c4
akhilgoud8 <meruguakhilgoud98@gmail.com>
2026-07-15 16:15:26 +0530

    added

---
+++ day-3/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-11"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
---
+++ day-3/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_id        = "ami-01edba92f9036f76e"
+instance_type = "t2.micro"
+tags          = "default-name"
